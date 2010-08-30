35971952d9df4e7c87151e8bb1ad6ffdc5ee8ff3
chris flöß <skeptikos@gmail.com>
2010-08-30 14:47:24 +0200

    import initial source

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlobalizeRailsOrg::Application.config.secret_token = '29135bcb9baa631f16bd7388df7ebbe683a810e6d35fb46c15c306e2c187f0fbbe639f5c6237d6cb2b45bf5f3c46bdacfc6b21041179b6089bc78fe793648a6d'
